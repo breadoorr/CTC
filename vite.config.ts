6c9c70c53032f378c8e94fbfe38619b7272cc268
ddorabble <daria.chystiakova@jetbrains.com>
2025-07-25 14:36:15 +0300

    [fix]

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,14 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/ctc/',
+  base: '/CTC/',
   server: {
     host: '0.0.0.0',
     port: 5173,
     strictPort: true,
-    allowedHosts: ['43a175f3b1f5.ngrok-free.app'],
+    allowedHosts: ['breadoorr.github.io'],
     cors: {
-      origin: ['https://43a175f3b1f5.ngrok-free.app', 'http://localhost:5173']
+      origin: ['https://43a175f3b1f5.ngrok-free.app', 'http://localhost:5173', 'https://breadoorr.github.io']
     },
     hmr: {
       clientPort: 443,
