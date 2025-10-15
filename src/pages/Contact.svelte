aae00825bf9eb3e8a03bd7f02aa709a3d79c3d25
breadoorr <dchystiakova@uclan.ac.uk>
2025-10-15 18:05:19 +0300

    [update]: changed email address

--- src/pages/Contact.svelte
+++ src/pages/Contact.svelte
@@ -37,14 +37,14 @@
           </div>
 
           <div class="contact-item">
-            <a href="mailto:info@ctc.cy"><div class="contact-icon">
+            <a href="mailto:www.ctc.cy@gmail.com"><div class="contact-icon">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M20 4H4C2.9 4 2.01 4.9 2.01 6L2 18C2 19.1 2.9 20 4 20H20C21.1 20 22 19.1 22 18V6C22 4.9 21.1 4 20 4ZM20 8L12 13L4 8V6L12 11L20 6V8Z" fill="currentColor"/>
               </svg>
             </div></a>
             <div class="contact-text">
               <h3>{$t('email')}</h3>
-              <p><a href="mailto:info@ctc.cy">info@ctc.cy</a></p>
+              <p><a href="mailto:www.ctc.cy@gmail.com">www.ctc.cy@gmail.com</a></p>
             </div>
           </div>
 
