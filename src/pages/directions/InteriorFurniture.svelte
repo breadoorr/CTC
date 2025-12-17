cbbc8bd05fd1dab318e4acffdf06180c8ff9ab2d
Chystiakova Daria <123006049+breadoorr@users.noreply.github.com>
2025-12-17 17:11:17 +0200

    Add image to src/pages/directions/InteriorFurniture.svelte via admin panel

--- src/pages/directions/InteriorFurniture.svelte
+++ src/pages/directions/InteriorFurniture.svelte
@@ -84,6 +84,11 @@
   // Images for this direction
   const images = [
       {
+        id: 34,
+        src: getAssetPath('/images/indoor/2025-12-17T15-11-14-912Z_shkaf1.jpg'),
+        alt: $currentLang === 'en' ? 'Interior Furniture' : 'Interior Furniture'
+      },
+      {
         id: 33,
         src: getAssetPath('/images/indoor/2025-12-17T15-08-49-398Z_koridor1.jpg'),
         alt: $currentLang === 'en' ? 'Interior Furniture' : 'Interior Furniture'
