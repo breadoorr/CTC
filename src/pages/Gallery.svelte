11740954ec5ae9fe1b280ffad367d346300c4a67
Chystiakova Daria <123006049+breadoorr@users.noreply.github.com>
2025-12-17 17:11:18 +0200

    Add image to gallery via admin panel

--- src/pages/Gallery.svelte
+++ src/pages/Gallery.svelte
@@ -85,6 +85,12 @@
   // Gallery images with translations for alt text
   $: rawGalleryImages = [
       {
+        id: 73,
+        src: '/images/indoor/2025-12-17T15-11-14-912Z_shkaf1.jpg',
+        alt: $currentLang === 'en' ? 'Interior Furniture' : 'Interior Furniture',
+        category: 'interior-furniture'
+      },
+      {
         id: 72,
         src: '/images/indoor/2025-12-17T15-08-49-398Z_koridor1.jpg',
         alt: $currentLang === 'en' ? 'Interior Furniture' : 'Interior Furniture',
